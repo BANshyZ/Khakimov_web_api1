ea1d4333ece80b86fe2dc13b6983b01c85676f1c
BANshyZ <extant1despair@gmail.com>
2024-12-27 09:13:14 +0300

    абоба

--- BlazorApp2/obj/Debug/net8.0/BlazorApp2.AssemblyInfo.cs
+++ BlazorApp2/obj/Debug/net8.0/BlazorApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b19fbfe311cbfa3713361a991c0d7d23d81bf8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
